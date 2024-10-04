0548d13deb0093b218cdf0bf3ffdd9118ed647cb
Nick Kocharhook <nick@kocharhook.com>
2024-10-04 16:36:24 -0400

    Fix build

--- pages/about.js
+++ pages/about.js
@@ -3,9 +3,7 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import ProTip from '../src/ProTip';
 import Link from '../src/Link';
-import Copyright from '../src/Copyright';
 
 export default function About() {
   return (
@@ -17,8 +15,6 @@ export default function About() {
         <Button variant="contained" component={Link} noLinkStyle href="/">
           Go to the main page
         </Button>
-        <ProTip />
-        <Copyright />
       </Box>
     </Container>
   );
